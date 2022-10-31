3c8519daa111487b1eb03af807552c1b95f7d91a
Gary Pennington <garypen@gmail.com>
2022-10-31 17:44:11 +0000

    use rustyline and dirs for a more professional experience
    Hold history in an approved place, add basic rustyline support.
    
    Next nice feature would be to add support for tab completion for
    alternatives. Not sure if that's viable, but may look into it.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::fs::File;
-use std::io::{self, BufRead, Write};
-use std::path::Path;
+use std::env;
+use std::fs::{metadata, File};
+use std::io::{self, BufRead};
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use trying::trie::TrieString;
 
 // The output is wrapped in a Result to allow matching on errors
@@ -15,46 +18,100 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn get_history_file() -> Option<PathBuf> {
+    dirs::preference_dir()
+        .and_then(|mut base| {
+            base.push("rspell");
+            // Note: Not create_dir_all(), because we don't want to create preference
+            // dirs if they don't exist.
+            if metadata(base.clone()).ok().is_none() {
+                std::fs::create_dir(base.clone()).ok()?
+            }
+            Some(base)
+        })
+        .map(|mut base| {
+            base.push("history.txt");
+            base
+        })
+}
+
+fn usage() {
+    eprintln!("usage: rspell <dictionary>");
+    std::process::exit(1);
+}
+
 fn main() -> Result<()> {
-    let mut trie = TrieString::<()>::new();
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("data/words_alpha.txt") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines.flatten() {
-            trie.insert(line.chars());
-        }
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        usage();
     }
+
+    let lines = read_lines(&args[1])?;
+    let mut trie = TrieString::<()>::new();
+
+    // Consumes the iterator, returns an (Optional) String
+    for line in lines.flatten() {
+        trie.insert(line.chars());
+    }
+
     println!(
-        "trie contains {}, over {} atoms",
+        "trie contains {} entries, using {} atoms",
         trie.count(),
         trie.atoms()
     );
-    let stdin = io::stdin();
-    let lines = stdin.lock().lines();
 
-    print!("word: ");
-    io::stdout().flush()?;
-    for line in lines {
-        let input = line?;
-        if trie.contains(input.chars()) {
-            println!("ok");
-        } else {
-            // There may be alternatives...
-            let alternatives = trie.get_alternatives(input.chars(), 5);
-            if !alternatives.is_empty() {
-                print!("try: ");
-                let mut sep = "";
-                for word in alternatives {
-                    print!("{}{}", sep, word);
-                    sep = ", ";
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new()?;
+    if let Some(file_location) = get_history_file() {
+        if let Err(e) = rl.load_history(&file_location) {
+            println!("error loading history: {}", e);
+        }
+    }
+    println!("terminate with ctrl-c or ctrl-d");
+    loop {
+        let readline = rl.readline("word: ");
+        match readline {
+            Ok(line) => {
+                if line.is_empty() {
+                    continue;
                 }
-                println!();
-            } else {
-                println!("not found");
+                rl.add_history_entry(line.as_str());
+                if trie.contains(line.chars()) {
+                    println!("ok");
+                } else {
+                    // There may be alternatives...
+                    let alternatives = trie.get_alternatives(line.chars(), 5);
+                    if !alternatives.is_empty() {
+                        print!("try: ");
+                        let mut sep = "";
+                        for word in alternatives {
+                            print!("{}{}", sep, word);
+                            sep = ", ";
+                        }
+                        println!();
+                    } else {
+                        println!("not found");
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("ctrl-c");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("ctrl-d");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
-        print!("word: ");
-        io::stdout().flush()?;
+    }
+    if let Some(file_location) = get_history_file() {
+        if let Err(e) = rl.save_history(&file_location) {
+            println!("error saving history: {}", e);
+        }
     }
     Ok(())
 }
